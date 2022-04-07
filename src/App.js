254d762d77c9bfffa7fcedc192a53f53e9f743e8
Artur Terziyan <ararad@rambler.ru>
2022-04-07 11:18:38 +0300

    fix: path for folders for each components in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import Header from './components/header';
-import MyProfile from './components/my-profile';
-import Sidebar from './components/sidebar';
+import Header from './components/Header/header';
+import MyProfile from './components/MyProfile/my-profile';
+import Sidebar from './components/Sidebar/sidebar';
 
 function App() {
   return (
