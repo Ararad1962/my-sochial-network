52cd2535fd048db5fe15f8e3ac6b812c895e9fc5
Artur Terziyan <ararad@rambler.ru>
2022-04-14 14:24:03 +0300

    feat: Add components ProfileInfo in my-Profile

--- src/components/MyProfile/my-profile.jsx
+++ src/components/MyProfile/my-profile.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import s from './my-profile.module.css'
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 
 const MyProfile = () => {
    return (
 
       <div className={s.myProfile}>
-         <div>
-            <img className='img-content' alt='someone' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
-         </div>
-         <div>
-            ava+description
-         </div>
+         <ProfileInfo />
          <MyPosts />
 
       </div>
