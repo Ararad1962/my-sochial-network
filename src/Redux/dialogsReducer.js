22ed2d039ee3cfc9fd9ba7eefa23683a48f7e80e
Artur Terziyan <ararad@rambler.ru>
2022-05-13 12:54:45 +0300

    refactor: Change If to Switch in Reducers

--- src/Redux/dialogsReducer.js
+++ src/Redux/dialogsReducer.js
@@ -3,20 +3,43 @@ const SEND_NEW_MESSAGE = 'SEND_NEW_MESSAGE'
 
 
 const dialogsReducer = (state, action) => {
-   if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      state.newMessageBody = action.newMessageBody;
+   switch (action.type) {
       
-   } else if (action.type === SEND_NEW_MESSAGE) {
-      state.MessageData.push({
-         id: 4,
-         message: state.newMessageBody
-      });
-      state.newMessageBody = action.newMessageBody;
-      state.newMessageBody = '';
+      case UPDATE_NEW_MESSAGE_BODY : {
+         state.newMessageBody = action.newMessageBody;
+         return state;
+      }
       
+      case SEND_NEW_MESSAGE : {
+         state.MessageData.push({
+            id: 4,
+            message: state.newMessageBody
+         });
+         state.newMessageBody = action.newMessageBody;
+         state.newMessageBody = '';
+         return state;
+      }
+        
+   
+      default:
+         return state;
    }
-
-return state;
 }
+   
+//    if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+//       state.newMessageBody = action.newMessageBody;
+      
+//    } else if (action.type === SEND_NEW_MESSAGE) {
+//       state.MessageData.push({
+//          id: 4,
+//          message: state.newMessageBody
+//       });
+//       state.newMessageBody = action.newMessageBody;
+//       state.newMessageBody = '';
+      
+//    }
+
+// return state;
+// }
 
 export default dialogsReducer;
